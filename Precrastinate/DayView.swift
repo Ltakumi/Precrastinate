0cad6eb5e0878eb649dc05bdf943e592edc4f55d
LTakumi <louis.lemoine@polytechnique.edu>
2023-12-02 18:10:22 +0900

    Added History view

---
+++ Precrastinate/DayView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+import CoreData
+
+struct DayView: View {
+    var date: Date
+    @Environment(\.managedObjectContext) private var viewContext
+
+    private var intervalsForDay: FetchRequest<Interval>
+
+    init(date: Date) {
+        self.date = date
+        let calendar = Calendar.current
+        let startOfDay = calendar.startOfDay(for: date)
+        let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)!
+
+        self.intervalsForDay = FetchRequest<Interval>(
+            entity: Interval.entity(),
+            sortDescriptors: [NSSortDescriptor(keyPath: \Interval.start, ascending: true)],
+            predicate: NSPredicate(format: "start >= %@ AND start < %@", argumentArray: [startOfDay, endOfDay])
+        )
+    }
+
+    var body: some View {
+        List {
+            ForEach(intervalsForDay.wrappedValue, id: \.self) { interval in
+                Text(interval.formatInterval())
+            }
+        }
+        .navigationTitle("Day: \(date.formatted(date: .long, time: .omitted))")
+    }
+}
+
+//#Preview {
+//    DayView()
+//}
--- Precrastinate/HistoryView.swift
+++ Precrastinate/HistoryView.swift
@@ -1,17 +1,32 @@
-//
-//  HistoryView.swift
-//  Precrastinate
-//
-//  Created by Louis Takumi on 2023/11/26.
-//
-
 import SwiftUI
 
 struct HistoryView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(
+        entity: Interval.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Interval.start, ascending: false)]
+    ) private var intervals: FetchedResults<Interval>
+
+    private var uniqueDays: [Date] {
+        let calendar = Calendar.current
+        let days = intervals.map { interval in
+            calendar.startOfDay(for: interval.start ?? Date())
+        }
+        return Array(Set(days)).sorted(by: { $0 > $1 })
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(uniqueDays, id: \.self) { day in
+                NavigationLink(destination: DayView(date: day)) {
+                    Text(day.formatted(date: .abbreviated, time: .omitted))
+                }
+            }
+            .navigationTitle("History")
+        }
     }
 }
+
 
 #Preview {
     HistoryView()
